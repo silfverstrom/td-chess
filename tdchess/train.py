42d1ffb2524dd1b46e004d3d3b044e112b215f12
Ubuntu <ubuntu@ip-172-31-9-198.eu-north-1.compute.internal>
2020-11-18 20:48:03 +0000

    added reguralisation

--- tdchess/train.py
+++ tdchess/train.py
@@ -17,9 +17,9 @@ def get_model():
 
     input1 = l.Input(shape=(774))
     x1 = l.Dense(2048, activation='relu')(input1)
-    #x1 = l.BatchNormalization()(x1)
+    x1 = l.BatchNormalization()(x1)
     x1 = l.Dense(64, activation='relu')(x1)
-    #x1 = l.BatchNormalization()(x1)
+    x1 = l.BatchNormalization()(x1)
     x1 = l.Dense(32, activation='relu')(x1)
 
     input2 = l.Input(shape=(15))
@@ -27,7 +27,7 @@ def get_model():
 
     x = l.Concatenate()([x1, x2])
     x = l.Dense(32)(x)
-    #x = l.Dropout(0.5)(x)
+    x = l.Dropout(0.5)(x)
 
     output =l.Dense(1, activation='linear')(x)
 
